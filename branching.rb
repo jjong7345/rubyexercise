249b6c234031d58b69d74a19c522c877163e03fe
Jong Suk Park <jjong7345@gmail.com>
2013-09-23 16:39:38 -0400

    branching exercise

--- branching.rb
+++ branching.rb
@@ -1,7 +1,18 @@
-puts 'Hello, waht\'s your name?'
+puts 'Hello, and welcome to seventh grade English'
+puts 'My name is Mr. Gabbard. And you name is....?'
 name = gets.chomp
 puts 'Hello, '+ name + '.'
 
-if name == "Jong"
-	puts 'What a lovely name!'
+if name == name.capitalize
+	puts 'please, take a seat' + name + '.'
+else 
+	puts name + '? You mean' + name.capitalize + '. right?' 
+	puts 'Don\'t you even know how to spell your name??'
+	replay = gets.chomp
+
+	if replay.downcase == "yes"
+		puts "Hummp! Well, sit down"
+	else 
+		puts "GET OUT!!"
+	end
 end
